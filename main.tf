813075fb06f3209aaa89b0043d0c7245f905792c
Glowbal <thomasskooi@live.nl>
2017-10-07 12:26:35 +0200

    Move manager and worker nodes to seperate terraform modules

--- main.tf
+++ main.tf
@@ -1,145 +1,41 @@
-provider "digitalocean" {
-  token = "${var.do_token}"
+module "managers" {
+  source   = "github.com/thojkooi/terraform-digitalocean-swarm-managers"
+  do_token = "${var.do_token}"
+
+  image  = "${var.manager_image}"
+  size   = "${var.manager_size}"
+  name   = "${var.manager_name}"
+  region = "${var.region}"
+  domain = "${var.domain}"
+
+  total_instances = "${var.total_managers + 1}"
+  user_data       = "${var.manager_user_data}"
+  tags            = "${var.manager_tags}"
+
+  ssh_keys          = "${var.worker_ssh_keys}"
+  provision_ssh_key = "${var.provision_ssh_key}"
+  provision_user    = "${var.provision_user}"
 }
 
-resource "digitalocean_droplet" "manager" {
-    ssh_keys           = "${var.manager_ssh_keys}"
-    image              = "${var.manager_os}"
-    region             = "${var.region}"
-    size               = "${var.manager_size}"
-    private_networking = true
-    backups            = false
-    ipv6               = false
-    tags               = ["${var.manager_tags}"]
-    user_data          = "${var.manager_user_data}"
-    count              = "${var.total_managers}"
-    name               = "${format("%s-%02d.%s.%s", var.manager_name, count.index + 1, var.region, var.domain)}"
+module "workers" {
+  source   = "github.com/thojkooi/terraform-digitalocean-swarm-workers"
+  do_token = "${var.do_token}"
 
-    connection {
-        type     = "ssh"
-        user     = "${var.provision_user}"
-        private_key = "${file("${var.provision_ssh_key}")}"
-        timeout  = "2m"
-    }
+  image  = "${var.worker_image}"
+  size   = "${var.worker_size}"
+  name   = "${var.worker_name}"
+  region = "${var.region}"
+  domain = "${var.domain}"
 
-    provisioner "remote-exec" {
-        inline = [
-            "while [ ! $(docker info) ]; do sleep 2; done",
-            # TODO: Handle failure during swarm init, only run this if manager node is not in a swarm
-            "if [ ${count.index} -eq 0 ]; then sudo docker swarm init --advertise-addr ${digitalocean_droplet.manager.0.ipv4_address_private}; exit 0; fi"
-        ]
-    }
-}
+  total_instances = "${var.total_workers}"
+  user_data       = "${var.worker_user_data}"
+  tags            = "${var.worker_tags}"
 
-data "external" "swarm_tokens" {
-    program = ["bash", "${path.module}/scripts/get-swarm-join-tokens.sh"]
-    query = {
-        host = "${digitalocean_droplet.manager.0.ipv4_address}"
-        user = "${var.provision_user}"
-        private_key = "${var.provision_ssh_key}"
-    }
-    depends_on = ["digitalocean_droplet.manager"]
-}
+  manager_public_ip  = "${element(module.managers.ipv4_addresses, 0)}"
+  manager_private_ip = "${element(module.managers.ipv4_addresses_private, 0)}"
+  join_token         = "${module.managers.worker_token}"
 
-#
-resource "null_resource" "bootstrap" {
-    depends_on = ["data.external.swarm_tokens"]
-    count       = "${var.total_managers}"
-
-    triggers {
-        cluster_instance_ids = "${join(",", digitalocean_droplet.manager.*.id)}"
-    }
-
-    connection {
-        host = "${element(digitalocean_droplet.manager.*.ipv4_address, count.index)}"
-        type     = "ssh"
-        user     = "${var.provision_user}"
-        private_key = "${file("${var.provision_ssh_key}")}"
-        timeout  = "2m"
-    }
-
-    provisioner "remote-exec" {
-        inline = [
-            "while [ ! $(docker info) ]; do sleep 2; done",
-            "if [ ${count.index} -gt 0 ] && [! sudo docker info | grep -q \"Swarm: active\" ]; then sudo docker swarm join --token ${data.external.swarm_tokens.result.manager} ${digitalocean_droplet.manager.0.ipv4_address_private}:2377; exit 0; fi"
-        ]
-    }
-}
-
-resource "digitalocean_droplet" "worker" {
-    ssh_keys           = "${var.worker_ssh_keys}"
-    image              = "${var.worker_os}"
-    region             = "${var.region}"
-    size               = "${var.worker_size}"
-    private_networking = true
-    backups            = false
-    ipv6               = false
-    user_data          = "${var.worker_user_data}"
-    tags               = ["${var.worker_tags}"]
-    count              = "${var.total_workers}"
-    name               = "${format("%s-%02d.%s.%s", var.worker_name, count.index + 1, var.region, var.domain)}"
-    depends_on         = ["digitalocean_droplet.manager"]
-
-    connection {
-        type        = "ssh"
-        user        = "${var.provision_user}"
-        private_key = "${file("${var.provision_ssh_key}")}"
-        timeout     = "2m"
-    }
-
-    provisioner "remote-exec" {
-        inline = [
-            "while [ ! $(docker info) ]; do sleep 2; done",
-            "sudo docker swarm join --token ${data.external.swarm_tokens.result.worker} ${digitalocean_droplet.manager.0.ipv4_address_private}:2377"
-        ]
-    }
-
-    # Handle clean up / destroy worker node
-    # drain worker on destroy
-    provisioner "remote-exec" {
-      when = "destroy"
-
-      inline = [
-        "docker node update --availability drain ${self.name}",
-      ]
-
-      on_failure = "continue"
-      # TODO: Handle downed manger node, move to a different node to drain worker node
-      connection {
-        type        = "ssh"
-        user        = "${var.provision_user}"
-        private_key = "${file("${var.provision_ssh_key}")}"
-        host        = "${digitalocean_droplet.manager.0.ipv4_address}"
-      }
-    }
-
-    # leave swarm on destroy
-    provisioner "remote-exec" {
-      when = "destroy"
-
-      inline = [
-        "docker swarm leave",
-      ]
-
-      on_failure = "continue"
-    }
-
-    # remove node on destroy
-    provisioner "remote-exec" {
-      when = "destroy"
-
-      inline = [
-        "docker node rm --force ${self.name}",
-      ]
-
-      on_failure = "continue"
-
-      connection {
-          type        = "ssh"
-          user        = "${var.provision_user}"
-          private_key = "${file("${var.provision_ssh_key}")}"
-          host        = "${digitalocean_droplet.manager.0.ipv4_address}"
-      }
-    }
-
+  ssh_keys          = "${var.worker_ssh_keys}"
+  provision_ssh_key = "${var.provision_ssh_key}"
+  provision_user    = "${var.provision_user}"
 }

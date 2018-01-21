39d6883681a01fc68fd998fc6c8cf357dae40217
Thomas Kooi <thomasskooi@live.nl>
2018-01-21 13:44:50 +0100

    Fix typo

--- examples/user-data/scripts/install-docker-ce.sh
+++ examples/user-data/scripts/install-docker-ce.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Insstall requirements for Docker
+# Install requirements for Docker
 yum -y update
 yum install -y yum-utils device-mapper-persistent-data lvm2
 
-# Add docker-ce repository and enable docker-edge
+# Add docker-ce repository and enable docker-ce-edge
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum-config-manager --enable docker-ce-edge
 yum -y makecache fast
